60c1a06b14fe669a5f4f1629a54cef019200e068
Marty <martychouette@gmail.com>
2026-08-30 11:48:54 -0400

    we untracked exported build outputs (Examples/*/Build - regenerate via MCP build_game)

--- Examples/Playground/Build/serve.py
+++
@@ -1,8 +0,0 @@
-import http.server, socketserver
-class H(http.server.SimpleHTTPRequestHandler):
-    extensions_map = {**http.server.SimpleHTTPRequestHandler.extensions_map,
-                      '.wasm': 'application/wasm', '.js': 'application/javascript'}
-PORT = 9090
-print(f'Serving at http://localhost:{PORT}')
-with socketserver.TCPServer(('', PORT), H) as httpd:
-    httpd.serve_forever()
